044fe8c1d27174cf95b79f8e4dc4dad90b483054
Johnny Kamigashima <johnny@jhk.app>
2024-04-20 21:20:50 -0300

    Variaveis

---
+++ src/conclusaoModulo3/variaveis.js
@@ -0,0 +1,90 @@
+/**
+ *  O JS possui vários tipos de variáveis e como não é uma linguagem fortemente tipada, ela aceita que seja atribuída um valor de um tipo diferente do que foi declarado inicialmente.
+ * 
+ * As variáveis inicializadas como constantes (const) são imutáveis, ou seja, não podem ser alteradas durante o programa. Porém desde que não haja uma atribuição direta o valor interno pode ser alterado indiretamente.
+ * 
+ * As variáveis inicializadas como variáveis (var) podem ser alteradas durante o programa e possuem um escopo ampliado, podendo ser lidas em partes fora do seu escopo.
+ * 
+ * O tipo comum de variável é inicializado com let, e este tipo pode ser alterado através de atribuições ao longo da execução do programa.
+ */
+
+/**
+ * Tipo de variáveis:
+ *  */
+
+// Booleano só possui dois estados: verdadeiro e falso
+const booleanoVerdadeiro = true;
+const booleanoFalso = false;
+
+// numérico pode ser de valores inteiros ou decimais
+const numeroInteiro = 100;
+const numeroDecimal = 10.5;
+const numeroNegativo = -10;
+
+// string é uma sequência de caracteres, geralmente conhecida como texto, ele também pode ser interpretada como um vetor linear de caracteres que podem formar frases ou não
+
+const frase = 'Isso é uma string';
+const uuid = '5b0b6a6c-9e9b-4f6d-9b3f-6f4b5d6c7e8f';
+const stringNumerica = '10';
+const stringData = '10/10/2020';
+
+// Undefined - indica que a variável foi declarada mas não teve um valor definido
+
+let indefinido
+
+// Null(nulo) - indica que a variável não possui nenhum valor
+
+const valornulo = null
+
+// Objeto - estruturas que representam objetos com propriedades, eles podem conter diversos outros tipos de variaveis
+
+const animalEstimacao = {
+    nome: 'Toothless',
+    idade: undefined,
+    cor: ['preto', 'dourado', 'castanho'],
+    vacinado: true,
+    garras: 20,
+    orelhas: null,
+    voar: () => {
+        console.log('Voando');
+    }
+}
+
+// Função é um tipo especial de variável que executa alguma ação ela pode ser atribuída como uma função anônina a uma variável com ou sem nome
+
+function funcao() { return true }
+const funcaoAnonima = function () { return true }
+const arrowFunction = () => { return true }
+
+// Vetor é uma lista de valores e podee possuir diversas dimensões (matriz) ela pode ser uma lista dentro de uma lista ou uma lista de objetos 
+
+const vetor = [1, 2, 3, 4, 5]
+const matriz = [[1, 2], [3, 4], [5, 6]]
+const vetorObjeto = [{ nome: 'Fulano', idade: 20 }, { nome: 'Ciclano', idade: 30 }]
+const matrizObjeto = [[{ nome: 'Fulano', idade: 20 }, { nome: 'Ciclano', idade: 30 }], [{ nome: 'Beltrano', idade: 40 }, { nome: 'Deltrano', idade: 50 }]]
+
+// Variavel tipo Data
+const data = new Date('2020-10-10')
+
+module.exports = {
+    booleanoVerdadeiro,
+    booleanoFalso,
+    numeroInteiro,
+    numeroDecimal,
+    numeroNegativo,
+    frase,
+    uuid,
+    stringNumerica,
+    stringData,
+    indefinido,
+    valornulo,
+    animalEstimacao,
+    funcao,
+    funcaoAnonima,
+    arrowFunction,
+    vetor,
+    matriz,
+    vetorObjeto,
+    matrizObjeto,
+    data
+}
---
+++ tests/conclusaoModulo3/variaveis.test.js
@@ -0,0 +1,89 @@
+const { describe, it } = require('mocha')
+const assert = require('node:assert')
+const { booleanoVerdadeiro,
+    booleanoFalso,
+    numeroInteiro,
+    numeroDecimal,
+    numeroNegativo,
+    frase,
+    uuid,
+    stringNumerica,
+    stringData,
+    indefinido,
+    valornulo,
+    animalEstimacao,
+    funcao,
+    funcaoAnonima,
+    arrowFunction,
+    vetor,
+    matriz,
+    vetorObjeto,
+    matrizObjeto,
+    data } = require('../../src/conclusaoModulo3/variaveis')
+
+describe('Conclusão Módulo 3 - Variáveis', () => {
+    it('Verifica se o booleano verdadeiro é mesmo boolean', () => {
+        assert.strict(typeof (booleanoVerdadeiro),
+            Boolean)
+    })
+    it('Verifica se o booleano falso é mesmo falso', () => {
+        assert.equal(booleanoFalso, false)
+    })
+    it('Verifica se o numero inteiro é igual a 100', () => {
+        assert.equal(numeroInteiro, 100)
+    })
+    it('Verifica se o numero decimal é número', () => {
+        assert.strict(typeof (numeroDecimal), Number)
+    })
+    it('Verifica se o numero negativo é igual a -10', () => {
+        assert.equal(numeroNegativo, -10)
+    })
+    it('Verifica se a frase é igual a "Isso é uma string"', () => {
+        assert.equal(frase, 'Isso é uma string')
+    })
+    it('Verifica se o UUID é string"', () => {
+        assert.strict(typeof (uuid), String)
+    })
+    it('Verifica se a string numerica é igual a "10"', () => {
+        assert.equal(stringNumerica, '10')
+    })
+    it('Verifica se a string data é igual a "10/10/2020"', () => {
+        assert.equal(stringData, '10/10/2020')
+    })
+    it('Verifica se o indefinido é igual a undefined', () => {
+        assert.equal(indefinido, undefined)
+    })
+    it('Verifica se o valornulo é igual a null', () => {
+        assert.equal(valornulo, null)
+    })
+    it('Verifica se o nome do animalEstimacao é igual a Toothless', () => {
+        assert.equal(animalEstimacao.nome, 'Toothless')
+    })
+    it('Verifica se o vacinado do animalEstimacao é igual a true', () => {
+        assert.ok(animalEstimacao.vacinado)
+    })
+    it('Verifica se a funcao retorna true', () => {
+        assert.ok(funcao())
+    })
+    it('Verifica se a funcaoAnonima retorna true', () => {
+        assert.ok(funcaoAnonima())
+    })
+    it('Verifica se a arrowFunction retorna true', () => {
+        assert.ok(arrowFunction())
+    })
+    it('Verifica se o tericeiro item do vetor é 3', () => {
+        assert.equal(vetor[2], 3)
+    })
+    it('Verifica se o segundo item do terceiro item da matriz é 6', () => {
+        assert.equal(matriz[2][1], 6)
+    })
+    it('Verifica se o nome do segundo Objeto do vetor de objeto é Ciclano', () => {
+        assert.equal(vetorObjeto[1].nome, 'Ciclano')
+    })
+    it('Verifica se a idade do segundo Objeto do segundo item da matriz de objeto é 50', () => {
+        assert.equal(matrizObjeto[1][1].idade, 50)
+    })
+    it('Verifica se a data é igual a 10/10/2020', () => {
+        assert.deepStrictEqual(data, new Date(Date.parse('2020-10-10')))
+    })
+})
